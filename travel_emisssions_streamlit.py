ca896c1ef90477c59a0e2fdd38a6d7edb92c9e77
Jenica Andersen <95931276+Jenica-A@users.noreply.github.com>
2023-08-08 17:18:35 -0600

    Update travel_emisssions_streamlit.py

--- travel_emisssions_streamlit.py
+++ travel_emisssions_streamlit.py
@@ -5,13 +5,18 @@ Streamlit Interactive Plot
 
 import streamlit as st
 
-
 st.title("Travel Emissions Calculator")
 st.write("'We can prepare ourselves, and our families for the changes we're seeing.. more words ' \n\n — Per Espen Stoknes")
-
 st.text("This app will allow you to enter miles traveled and tell you CO2e emissions more words")
 
-miles = st.number_input(label = "Insert the distance traveled in miles", min_value = 0, max_value = 10000)
+options = st.multiselect(
+    'What modes of transportation did you use',
+    ['Personal vehicle', 'Bus (not including your own personal bus)', 'Flight', 'Rail', 'Boat'])
+
+st.write('You selected:', options)
+
+if options == 'Personal vehicle':
+    miles = st.number_input(label = "Insert the distance traveled in miles", min_value = 0, max_value = 10000)
 st.write('The current distance is ', miles)
 def emiss_calc(miles):
     co2 = miles * 404
@@ -19,6 +24,7 @@ def emiss_calc(miles):
 
 co2_g = emiss_calc(miles) 
 co2_t = round((co2_g / 907200),3)
+
 
 
 
