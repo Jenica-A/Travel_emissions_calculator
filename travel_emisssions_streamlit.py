9160a9f3c2d2c497f937eb207305ef6f49df9645
Jenica Andersen <95931276+Jenica-A@users.noreply.github.com>
2023-08-08 08:20:34 -0600

    Update travel_emisssions_streamlit.py

--- travel_emisssions_streamlit.py
+++ travel_emisssions_streamlit.py
@@ -10,4 +10,3 @@ st.title("New York Times Topic Modelling")
 st.write("'More information is always better than less. When people know the reason things are happening, even if it's bad news, they can adjust their expectations and react accordingly. Keeping people in the dark only serves to stir negative emotions.' \n\n — Simon Sinek")
 
 st.text("This app will allow you to choose a range of dates, and a section from the New York Times and generate a wordcloud image, based on word frequency wihtin article excerpts")
-"""
