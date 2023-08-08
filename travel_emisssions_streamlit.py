f3a35f6ba6a350128e115de29b7a47e69b8c715b
Jenica Andersen <95931276+Jenica-A@users.noreply.github.com>
2023-08-08 08:27:59 -0600

    Update travel_emisssions_streamlit.py

--- travel_emisssions_streamlit.py
+++ travel_emisssions_streamlit.py
@@ -6,7 +6,7 @@ Streamlit Interactive Plot
 import streamlit as st
 
 
-st.title("New York Times Topic Modelling")
-st.write("'More information is always better than less. When people know the reason things are happening, even if it's bad news, they can adjust their expectations and react accordingly. Keeping people in the dark only serves to stir negative emotions.' \n\n — Simon Sinek")
+st.title("Travel Emissions Calculator")
+st.write("'We can prepare ourselves, and our families for the changes we're seeing.. more words ' \n\n — Per Espen Stoknes")
 
-st.text("This app will allow you to choose a range of dates, and a section from the New York Times and generate a wordcloud image, based on word frequency wihtin article excerpts")
+st.text("This app will allow you to enter miles traveled and tell you CO2e emissions more words")
